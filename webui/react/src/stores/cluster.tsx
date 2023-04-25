fcaeb44e90b6d9e6bc8824c71f07997f81ee9a26
Ashton G <ashton.galloway@hpe.com>
2023-04-25 13:27:12 -0400

    fix: Fix cluster store polling (#6611)
    This fixes an issue where the cluster store would poll without waiting
    for the previous request to return. We fix this by decoupling the
    standalone request functions (one of which is used in a modal) from the
    overall polling logic.

--- webui/react/src/stores/cluster.tsx
+++ webui/react/src/stores/cluster.tsx
@@ -85,6 +85,9 @@ const clusterStatusText = (
   )}%`;
 };
 
+const updateIfChanged = <T, V extends WritableObservable<T>>(o: V, next: T) =>
+  o.update((prev) => (isEqual(prev, next) ? prev : next));
+
 class ClusterStore extends PollingStore {
   #agents: WritableObservable<Loadable<Agent[]>> = observable(NotLoaded);
   #resourcePools: WritableObservable<Loadable<ResourcePool[]>> = observable(NotLoaded);
@@ -136,21 +139,19 @@ class ClusterStore extends PollingStore {
 
     getAgents({}, { signal: signal ?? canceler.signal })
       .then((response) => {
-        const next = Loaded(response);
-        this.#agents.update((prev) => (isEqual(prev, next) ? prev : next));
+        updateIfChanged(this.#agents, Loaded(response));
       })
       .catch(handleError);
 
     return () => canceler.abort();
   }
 
-  public fetchResourcePools(signal?: AbortSignal) {
+  public fetchResourcePools(signal?: AbortSignal): () => void {
     const canceler = new AbortController();
 
     getResourcePools({}, { signal: signal ?? canceler.signal })
       .then((response) => {
-        const next = Loaded(response);
-        this.#resourcePools.update((prev) => (isEqual(prev, next) ? prev : next));
+        updateIfChanged(this.#resourcePools, Loaded(response));
       })
       .catch(handleError);
 
@@ -158,10 +159,11 @@ class ClusterStore extends PollingStore {
   }
 
   public async poll() {
-    await Promise.all([
-      this.fetchResourcePools(this.canceler?.signal),
-      this.fetchAgents(this.canceler?.signal),
-    ]);
+    const agentRequest = getAgents({}, { signal: this.canceler?.signal });
+    const poolsRequest = getResourcePools({}, { signal: this.canceler?.signal });
+    const [agents, resourcePools] = await Promise.all([agentRequest, poolsRequest]);
+    updateIfChanged(this.#resourcePools, Loaded(resourcePools));
+    updateIfChanged(this.#agents, Loaded(agents));
   }
 }
 
